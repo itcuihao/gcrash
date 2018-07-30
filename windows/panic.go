c682d337b0ad784df5c88ea7f729c9ff75a015f0
itcuihao <it_cuihao@163.com>
2018-07-30 21:45:01 +0800

    update

--- windows/panic.go
+++ windows/panic.go
@@ -8,29 +8,30 @@ import (
 )
 
 const (
-	kernel32dll = "kernel32.dll"
+	kdll = "kernel32.dll"
+	ssh  = "SetStdHandle"
 )
 
-var globalFile *os.File
+var globalf *os.File
 
 // Catch catch panic
 // path is output address
 func Catch(path string) error {
-
+	// windows uncomment
 	file, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
-	globalFile = file
+	globalf = file
 	if err != nil {
 		return err
 	}
 
-	t := fmt.Sprintf("time: %s", time.Now().Format("2006-01-02 15:04:05"))
+	t := fmt.Sprintf("panic time: %s", time.Now().Format("2006-01-02 15:04:05"))
 	file.Write([]byte(t))
 	file.Write([]byte("\r\n"))
 
-	kernel32 := syscall.NewLazyDLL(kernel32dll)
-	setStdHandle := kernel32.NewProc("SetStdHandle")
-	sh := syscall.STD_ERROR_HANDLE
-	v, _, err := setStdHandle.Call(uintptr(sh), uintptr(file.Fd()))
+	lp := syscall.NewLazyDLL(kdll).NewProc(ssh)
+
+	seh := syscall.STD_ERROR_HANDLE
+	v, _, err := lp.Call(uintptr(seh), uintptr(file.Fd()))
 	if v == 0 {
 		return err
 	}
